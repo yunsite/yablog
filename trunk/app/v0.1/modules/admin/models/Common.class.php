6d9ada1b4cf568dd5c7b9433e6da8b1363d2a49c
msl-138@163.com <msl-138@163.com@352b41c2-25d5-5ce0-cd12-f441bac9e1a4>
2013-06-21 08:56:39 +0000

    1.增加 _checkCacheExist方法根据主键验证缓存是否存在 2.优化_checkUnique,支持parent_id字段设置

--- trunk/app/v0.1/modules/admin/models/Common.class.php
+++ trunk/app/v0.1/modules/admin/models/Common.class.php
@@ -13,6 +13,21 @@
 
 class CommonModel extends BaseModel {
     /**
+     * 根据主键验证缓存是否存在
+     *
+     * @author          mrmsl <msl-138@163.com>
+     * @date            2013-06-21 11:52:03
+     *
+     * @param int       $id         主键id
+     * @param string    $filename   缓存文件名,默认CONTROLLER_NAME
+     *
+     * @return bool true验证成功,否则false
+     */
+    protected function _checkCacheExists($id, $filename = CONTROLLER_NAME) {
+        return $id && $this->_getCache($id, $filename) ? true : false;
+    }
+
+    /**
      * 从缓存文件验证唯一字段
      *
      * @author          mrmsl <msl-138@163.com>
@@ -27,12 +42,13 @@ class CommonModel extends BaseModel {
      * @return mixed true验证成功。否则，如果未输入，返回提示信息，否则返回false
      */
     protected function _checkUnique($name, $data, $field_name, $lang_name = null, $parent_id = false) {
-        $pk_field = $this->getPk();
+        $parent_id_column   = is_numeric($parent_id) ? 'parent_id' : $parent_id;
+        $pk_field           = $this->getPk();
 
         if ('' === $name || !isset($data[$pk_field])) {//如果未输入，提示输入
             return false;
         }
-        elseif ($parent_id && !isset($data['parent_id'])) {
+        elseif ($parent_id && !isset($data[$parent_id_column])) {
             return false;
         }
 
@@ -47,11 +63,11 @@ class CommonModel extends BaseModel {
 
 
         if ($parent_id) {
-            $parent_id = $data['parent_id'];
+            $parent_id = $data[$parent_id_column];
 
             foreach ($caches as $id => $item) {
 
-                if ($name == $item[$field_name] && $pk_value != $id && $item['parent_id'] == $parent_id) {
+                if ($name == $item[$field_name] && $pk_value != $id && $item[$parent_id_column] == $parent_id) {
                     return L($lang_name . ',EXIST');
                 }
             }
