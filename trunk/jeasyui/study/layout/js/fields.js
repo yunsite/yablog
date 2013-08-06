652025651efc5962879eaee24be8c4f70fcefae7
msl-138@163.com <msl-138@163.com@352b41c2-25d5-5ce0-cd12-f441bac9e1a4>
2013-08-06 10:30:20 +0000

    debug版及jeasyui 默认defaults 重写

---
+++ trunk/jeasyui/study/layout/js/fields.js
@@ -0,0 +1,35 @@
+/**
+ * 字段
+ *
+ * @file            fields.js
+ * @version         0.1
+ * @copyright       Copyright (c) 2013 {@link http://www.yablog.cn yablog} All rights reserved
+ * @license         http://www.apache.org/licenses/LICENSE-2.0.html Apache License 2.0
+ * @author          mrmsl <msl-138@163.com>
+ * @date            2013-08-06 12:01:29
+ * @lastmodify      $Date$ $Author$
+ */
+
+define('fields', function(require, exports, module) {
+    var fields = {};
+    fields.matchMode = {
+        width: 100,
+        panelHeight: 100,
+        data: [{
+            text: '完全匹配',
+            value: 'eq',
+            selected: true
+        }, {
+            text: '左匹配',
+            value: 'leq'
+        }, {
+            text: '右匹配',
+            value: 'req'
+        }, {
+            text: '模糊匹配',
+            value: 'like'
+        }]
+    };
+
+    module.exports = fields;
+});
