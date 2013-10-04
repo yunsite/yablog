18938bfa7b84052f1a0c9d3cbcac277b268a780b
msl-138 <msl-138@163.com>
2013-10-04 06:36:29 +0000

    后台核心加载js文件

--- app/v0.2/modules/admin/include/required_js.php
+++ app/v0.2/modules/admin/include/required_js.php
@@ -13,30 +13,21 @@
  */
 
 return array(
-    'util/common.js',
-    'util/override.js',
-    'util/Yab.History.js',
-    'util/Yab.Application.js',
-    'util/Yab.Field.js',//表单域 by mrmsl on 2012-12-11 15:56:15
-    'store/Yab.store.Admin.js',
-    'store/Yab.store.Role.js',
-    'store/Yab.store.Tree.js',
-    'store/Yab.store.Area.js',//国家地区 by mrmsl 22:05 2012-7-18
-    'store/Yab.store.Category.js',//博客分类 by mrmsl on 2013-03-21 16:26:55
-    'ux/Yab.ux.RoleCombo.js',
-    'ux/Yab.ux.TreePicker.js',//下拉树 by mrmsl on 2012-08-02 18:25:52
-    'ux/Yab.ux.Form.js',//表单 by mrmsl on 2012-12-11 15:55:54
-    'ux/Yab.ux.Grid.js',//普通列表扩展 by mrmsl on 2012-12-18 11:32:51
-    'ux/Yab.ux.TreeGrid.js',//树列表扩展 by mrmsl on 2012-12-18 11:32:51
-    'view/Yab.view.Viewport.js',
-    'view/Yab.view.Tabs.js',
-    'view/Yab.view.Index.js',
-    'view/Yab.view.Center.js',
-    'view/Yab.view.Header.js',
-    'view/Yab.view.Tree.js',
-    'controller/Yab.controller.Base.js',//底层控制器 by mrmsl on 2012-07-28 09:04:24
-    'controller/Yab.controller.Tree.js',
-    'controller/Yab.controller.Tabs.js',
-    'controller/Yab.controller.Index.js',
-    'controller/Yab.controller.Login.js',
+    //第三方核心
+    'jquery-1.10.2.js'      => COMMON_IMGCACHE . 'js/jquery/',          //jquery
+    'underscore.js'         => COMMON_IMGCACHE . 'js/underscore/',      //underscore.js
+    'backbone.js'           => COMMON_IMGCACHE . 'js/backbone/',        //backbone.js
+    'ligerui.js'            => COMMON_IMGCACHE . 'js/ligerui/js/',      //ligerui框架
+    'base/base.js'          => COMMON_IMGCACHE . 'js/',                 //Base类
+    'sea.js'                => COMMON_IMGCACHE . 'js/seajs/',           //seajs
+
+    //通用js
+    'common.js'             => COMMON_IMGCACHE . 'js/',                  //通用js
+
+    //yablog
+    'global.js'             => ADMIN_IMGCACHE . 'js/',                  //整站全局js
+    'base.js'               => ADMIN_IMGCACHE . 'js/',                  //基础库
+    'router.js'             => ADMIN_IMGCACHE . 'js/',                  //路由
+    'tree.js'               => ADMIN_IMGCACHE . 'js/',                  //导航菜单
+    'tabs.js'               => ADMIN_IMGCACHE . 'js/',                  //导航菜单
 );
