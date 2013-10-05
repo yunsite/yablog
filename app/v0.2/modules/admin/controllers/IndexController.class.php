4ad8e8ba75fc4dc12e137e187916b8decb3e5ac0
msl-138 <msl-138@163.com>
2013-10-05 07:59:39 +0000

    增加include/include_template.php,后台首页及登陆页共用加载js,css文件及模板

--- app/v0.2/modules/admin/controllers/IndexController.class.php
+++ app/v0.2/modules/admin/controllers/IndexController.class.php
@@ -17,6 +17,7 @@ class IndexController extends CommonController {
      * @var bool $_auto_check_priv true自动检测权限。默认false
      */
     protected $_auto_check_priv = false;
+
     /**
      * @var bool $_init_model true实例对应模型。默认false
      */
@@ -25,9 +26,8 @@ class IndexController extends CommonController {
     /**
      * 管理中心。如果未登陆，跳转至登陆页
      *
-     * @author          mrmsl
+     * @author          mrmsl <msl-138@163.com>
      * @date            2012-07-02 11:12:49
-     * @lastmodify      2013-03-29 15:58:41 by mrmsl
      *
      * @return void 无返回值。如果未登陆跳转至登陆页
      */
@@ -40,32 +40,6 @@ class IndexController extends CommonController {
 
         $admin_priv = strtolower(json_encode(array_values($this->_role_info['priv'])));
         $role_info  = $this->cache($admin_info['role_id'], 'Role');
-        //css文件
-        $css_file  = $this->_loadTimeScript('START_TIME');//,extjs/v4.1.1/resources/css/ext-patch.css
-        $css_file .= css('extjs/v4.1.1a//resources/css/ext-all-gray.css,extjs/v4.1.1a/resources/css/ext-patch.css', COMMON_IMGCACHE);
-        $css_file .= css('app.css', ADMIN_IMGCACHE . 'css/');
-        $js_file   = $this->_loadTimeScript('LOAD_CSS_TIME');
-        $js_file  .= js('', true, COMMON_IMGCACHE . 'extjs/v4.1.1a/');
-        $js_file  .= $this->_loadTimeScript('LOAD_EXT_TIME');
-        $js_file  .= js('System.js', false, '/static/js/');
-        $js_file  .= js('System.sys_base_admin_entry = "' . WEB_ADMIN_ENTRY . '"', 'script');//后台入口
-        $js_file  .= js(MODULE_NAME . '.' . LANG . '.js', false, '/static/js/lang/') .
-        //ext语言包
-        ('en' != LANG ? js('ext-lang-' . LANG . '.js', false, '/static/js/lang/') : '');
-
-        if (APP_DEBUG) {
-            $js_arr = include(APP_PATH . 'include/required_js.php');
-
-            $js_file .= js($js_arr, false, ADMIN_IMGCACHE . 'app/');
-        }
-        else {
-            $js_file .= js('app/pack/app.js', false);
-        }
-
-        //$js_file  .= js('editor_config.js', false, COMMON_IMGCACHE . 'ueditor/v1.2.5/');
-
-        $js_file  .= $this->_loadTimeScript('LOAD_JS_TIME');
-
-        include(TEMPLATE_FILE);
+        require(APP_PATH . 'include/include_template.php');
     }//end indexAction
 }
---
+++ app/v0.2/modules/admin/include/include_template.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * 加载模板
+ *
+ * @file            include_template.php
+ * @package         Yab\Admin
+ * @version         0.2
+ * @copyright       Copyright (c) 2013 {@link http://www.yablog.cn yablog} All rights reserved
+ * @license         http://www.apache.org/licenses/LICENSE-2.0.html Apache License 2.0
+ * @author          mrmsl <msl-138@163.com>
+ * @date            2013-10-05 15:51:19
+ * @lastmodify      $Date$ $Author$
+ */
+
+//css文件
+$css_file   = css('gray-all.css', COMMON_IMGCACHE . 'js/ligerui/skins/Gray/css/');//ligerui样式
+
+//js文件
+$js_file    = js('System.js,lang/' . MODULE_NAME . '.' . LANG . '.js', '/static/js/');//系统信息,语言包
+$js_file   .= js('System.sys_base_admin_entry = "' . WEB_ADMIN_ENTRY . '"', 'script');//后台入口
+
+if (APP_DEBUG) {
+    $require_js = include(APP_PATH . 'include/required_js.php');
+    $js_file .= js($require_js);
+
+}
+else {
+    $js_file .= js('all.min.js', ADMIN_IMGCACHE . 'js/core/');
+}
+
+require(TEMPLATE_FILE);
